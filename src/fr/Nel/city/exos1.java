78a338759e64d9eb881750681624bf391fa77ae9
Donovan Seulin <nel640@hotmail.fr>
2024-01-09 11:33:01 +0100

    ajout fichier class

---
+++ src/fr/Nel/city/exos1.java
@@ -0,0 +1,16 @@
+package fr.Nel.city;
+
+public class exos1 
+{
+	public static void main(String[] args) 
+	{
+		City city1 = new City( "soustons" , "france" , 2000);
+		System.out.println(city1 + "\n");
+		City city2 = new City ( "toulouse" , "france" , 10000);
+		System.out.println(city2 + "\n");
+		City city3 = new City("osaka", "japan", 500000);
+		System.out.println(city3);
+	}
+	
+
+}
