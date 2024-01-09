681e65c06384ca49cf7369d61f5fbdd3f9a03df9
Donovan Seulin <nel640@hotmail.fr>
2024-01-09 11:32:14 +0100

    ajout fichier java

---
+++ src/fr/Nel/city/City.java
@@ -0,0 +1,22 @@
+package fr.Nel.city;
+
+public class City {
+		
+	public String city;
+	public String country;
+	public int numberPoeple;
+	
+	public City ( String city , String country , int numberPoeple)
+	{
+		this.city = city;
+		this.country = country;
+		this.numberPoeple = numberPoeple;
+	}
+	
+	public String toString()
+	{
+		return  "city : " + this.city + "\n" +
+				"country : " + this.country + "\n" +
+				"nombre d'habitant : " + this.numberPoeple;
+	}
+}
